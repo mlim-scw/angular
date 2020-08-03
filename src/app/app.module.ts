39c0eafe19d346170de3a7e50174f90dd491c36a
Mark Lim <mlim@securecodewarrior.com>
2020-08-04 09:15:34 +1000

    Using Bootstrap for Styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
